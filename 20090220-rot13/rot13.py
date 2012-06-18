2459ee14e670e8cf1e74c470a7e9a9c69787fe20
Valia Vaneeva <fattie@gmail.com>
2012-06-18 11:13:57 -0700

    http://programmingpraxis.com/2009/02/20/rot13/

---
+++ 20090220-rot13/rot13.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+def rot13():
+    pass
+
+class TestROT13(unittest.TestCase):
+
+    def test_rot13(self):
+        self.assertEqual()
+
+
+if __name__ == '__main__':
+    unittest.main()

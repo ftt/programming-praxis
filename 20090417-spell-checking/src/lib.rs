94b882f34fa5b0071798845587a551508ee3965c
Valentina Vaneeva <vavane@microsoft.com>
2018-11-19 14:29:52 -0800

    https://programmingpraxis.com/2009/04/17/spell-checking/

--- 20090417-spell-checking/src/lib.rs
+++ 20090417-spell-checking/src/lib.rs
@@ -1,38 +1,68 @@
-struct Trie {
-    children: Vec<TrieNode>,
-}
+use std::collections::HashMap;
 
-struct TrieNode {
-    letter: char,
+#[derive(Default)]
+pub struct TrieNode {
     is_word_end: bool,
-    children: Vec<TrieNode>,
+    children: HashMap<char, TrieNode>,
 }
 
 impl TrieNode {
-    fn new(letter: char, is_word_end: bool) -> TrieNode {
+    pub fn new() -> TrieNode {
         TrieNode {
-            letter: letter,
-            is_word_end: is_word_end,
-            children: Vec::new(),
+            is_word_end: false,
+            children: HashMap::new(),
         }
     }
-}
 
-impl Trie {
-    fn new() -> Trie {
-        Trie {
-            children: Vec::new(),
+    fn mark_word_end(&mut self) {
+        self.is_word_end = true;
+    }
+
+    pub fn add_word(&mut self, word: &str) {
+        if let Some(letter) = word.chars().nth(0) {
+            let node = self.children.entry(letter).or_insert_with(TrieNode::new);
+            node.add_word(&word[1..]);
+        } else {
+            self.mark_word_end();
+        }
+    }
+
+    pub fn check_word(&self, word: &str) -> bool {
+        if let Some(letter) = word.chars().nth(0) {
+            if let Some(node) = self.children.get(&letter) {
+                node.check_word(&word[1..])
+            } else {
+                false
+            }
+        } else {
+            self.is_word_end
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use ::{Trie, TrieNode};
+    use TrieNode;
 
     #[test]
     fn it_works() {
-        let trie = Trie::new();
-        let trie_node = TrieNode::new('a', true);
+        let mut trie_node = TrieNode::new();
+
+        trie_node.add_word("cart");
+        assert_eq!(trie_node.check_word("cart"), true);
+
+        assert_eq!(trie_node.check_word(""), false);
+        trie_node.add_word("");
+        assert_eq!(trie_node.check_word(""), true);
+
+        trie_node.add_word("car");
+        trie_node.add_word("cat");
+        trie_node.add_word("dog");
+        trie_node.add_word("quarter");
+        assert_eq!(trie_node.check_word("car"), true);
+        assert_eq!(trie_node.check_word("cat"), true);
+        assert_eq!(trie_node.check_word("dog"), true);
+        assert_eq!(trie_node.check_word("do"), false);
+        assert_eq!(trie_node.check_word("qarter"), false);
     }
 }
